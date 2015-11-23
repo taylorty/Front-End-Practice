e06f8b3b14f1aff8bf9f41f8ddf90d4d53f6726b
taylorty <taylor4869@gmail.com>
2015-11-23 00:48:30 -0500

    Initial commit

---
+++ Meteor.js /my_first_app/my_first_app.js
@@ -0,0 +1,28 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  Template.time.helpers({
+    time: new Date()
+    }
+  );
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
