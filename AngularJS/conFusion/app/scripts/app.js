1701234972944be0a62a6f2ad6e6dc5fe2ee7b52
taylorty <taylor4869@gmail.com>
2015-12-31 18:17:07 -0500

    Add Angular  service

--- AngularJS/conFusion/app/scripts/app.js
+++ AngularJS/conFusion/app/scripts/app.js
@@ -1,43 +1,48 @@
 'use strict';
+
 angular.module('confusionApp', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
+
         // route for the home page
             .state('app', {
-                url: '/',
-                views: {
-                    'header': {
-                        templateUrl: 'views/header.html',
-                    },
-                    'content': {
-                        template: '<h1>To be Completed</h1>',
-                        controller: 'IndexController'
-                    },
-                    'footer': {
-                        templateUrl: 'views/footer.html',
-                    }
+            url: '/',
+            views: {
+                'header': {
+                    templateUrl: 'views/header.html',
+                },
+                'content': {
+                    templateUrl: 'views/home.html',
+                    controller: 'IndexController'
+                },
+                'footer': {
+                    templateUrl: 'views/footer.html',
                 }
-            })
-            // route for the aboutus page
-            .state('app.aboutus', {
-                url: 'aboutus',
-                views: {
-                    'content@': {
-                        template: '<h1>To be Completed</h1>',
-                        controller: 'AboutController'
-                    }
+            }
+
+        })
+
+        // route for the aboutus page
+        .state('app.aboutus', {
+            url: 'aboutus',
+            views: {
+                'content@': {
+                    templateUrl: 'views/aboutus.html',
+                    controller: 'AboutController'
                 }
-            })
-            // route for the contactus page
-            .state('app.contactus', {
-                url: 'contactus',
-                views: {
-                    'content@': {
-                        templateUrl: 'views/contactus.html',
-                        controller: 'ContactController'
-                    }
+            }
+        })
+
+        // route for the contactus page
+        .state('app.contactus', {
+            url: 'contactus',
+            views: {
+                'content@': {
+                    templateUrl: 'views/contactus.html',
+                    controller: 'ContactController'
                 }
-            })
+            }
+        })
 
         // route for the menu page
         .state('app.menu', {
@@ -60,5 +65,6 @@ angular.module('confusionApp', ['ui.router'])
                 }
             }
         });
+
         $urlRouterProvider.otherwise('/');
     });
