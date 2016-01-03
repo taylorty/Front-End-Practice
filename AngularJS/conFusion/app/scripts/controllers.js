f23f5702da5549534addc9b7acfd13dc9dddbee4
taylorty <taylor4869@gmail.com>
2016-01-03 18:26:46 -0500

    Use End-to-End test Angular Applications

--- AngularJS/conFusion/app/scripts/controllers.js
+++ AngularJS/conFusion/app/scripts/controllers.js
@@ -131,7 +131,7 @@ angular.module('confusionApp')
             author: "",
             date: ""
         };
-    }
+    };
 }])
 
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
